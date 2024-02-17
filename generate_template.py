a3524e67c8676fb4be0ddf08654e76c8c2621e4a
jimmfan <jimmfan@users.noreply.github.com>
2024-02-17 19:21:53 +0000

    testing multiple templates

--- generate_template.py
+++ generate_template.py
@@ -1,39 +1,65 @@
 import click
 from cookiecutter.main import cookiecutter
 
+
 # Define the path to your cookiecutter template
 @click.command()
-# @click.option('--template_path', default='./', help='Path to your cookiecutter template.')
-@click.option('--project_name', prompt='Project Name', default='ml_project', help='Project Name, used as the project directory name.')
-@click.option('--author', prompt='Author name', default='jimmfan', help='The name of the project author.')
-@click.option('--email', prompt='Author email', default='jimmfan@github.com', help='The email of the project author.')
-@click.option('--python_version', prompt='Python version', default='3.10', help='The Python version to use for the project.')
-@click.option('--output_dir', prompt='Output directory', default='../', help='Output directory to where to create the template.')
-
+@click.option(
+    "--template_path",
+    prompt="Template Path",
+    default="cookiecutter-templates/template1",
+    help="Path to your cookiecutter template.",
+)
+@click.option(
+    "--project_name",
+    prompt="Project Name",
+    default="ml_project",
+    help="Project directory name.",
+)
+@click.option(
+    "--author",
+    prompt="Author name",
+    default="jimmfan",
+    help="The name of the project author.",
+)
+@click.option(
+    "--email",
+    prompt="Author email",
+    default="jimmfan@github.com",
+    help="The email of the project author.",
+)
+@click.option(
+    "--python_version",
+    prompt="Python version",
+    default="3.10",
+    help="The Python version to use for the project.",
+)
+@click.option(
+    "--output_dir",
+    prompt="Output directory",
+    default="../",
+    help="Output directory to where to create the template.",
+)
 def create_project(
-    # template_path, 
-    project_name, 
-    author, 
-    email, 
-    python_version, 
-    output_dir
+    template_path, project_name, author, email, python_version, output_dir
 ):
     # Call cookiecutter to create the project using the collected inputs
     cookiecutter(
-        './', # template_path, 
+        template_path,
         output_dir=output_dir,
         no_input=True,
         extra_context={
-            'project_name': project_name,
-            'author': author,
-            'email': email,
-            'python_version': python_version
+            "project_name": project_name,
+            "author": author,
+            "email": email,
+            "python_version": python_version
             # Add other variables to pass to cookiecutter here
         },
-        overwrite_if_exists=True
+        overwrite_if_exists=True,
     )
 
-    click.echo(f'Project {project_name} created successfully.')
+    click.echo(f"Project {project_name} created successfully.")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     create_project()
